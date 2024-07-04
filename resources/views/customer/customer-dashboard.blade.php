cc9a1e8b65f35db924ae9edd0a3e276183aec17f
SJ Mosquite <94162580+jeyone17@users.noreply.github.com>
2024-07-04 05:44:59 +0000

    edited customer dashboard

--- resources/views/customer/customer-dashboard.blade.php
+++ resources/views/customer/customer-dashboard.blade.php
@@ -1,1 +1,42 @@
 @extends('layouts.customer_layout')
+
+@section('title', 'Products')
+
+@section('contents')
+
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">List of Products</h5>
+                        <!-- Table with stripped rows -->
+                        <table class="table datatable">
+                            <thead>
+                                <tr>
+                                    <th>Image</th>
+                                    <th>Rice type</th>
+                                    <th>Unit</th>
+                                    <th>Selling Price</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td>
+                                            <a href="{{ route('cart') }}" class="btn btn-success mx-2" >Add to Cart</a>
+                                        </td>
+                                    </tr>
+                            </tbody>
+                        </table>
+                        <!-- End Table with stripped rows -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
